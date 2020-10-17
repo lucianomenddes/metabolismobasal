e400f40ce7f43581187b8a08cf6a733beb6f654b
Diego Buriti <bs.diego97@gmail.com>
2020-10-17 19:03:44 -0300

    Added some divs on template string and a smooth transtion.

--- public/scripts.js
+++ public/scripts.js
@@ -1,30 +1,18 @@
-const url = new URL(window.location);
-const locale = url.searchParams.get("language") || "pt-BR";
-const elements = document.querySelectorAll("[data-i18n]");
-let lang = "";
-if (locale == "es") {
-  lang = esLangConfig;
-}
-for (let element of elements) {
-  const key = element.getAttribute("data-i18n");
-  element.innerText = lang[key] || key;
-}
-
-const formulario = document.getElementById('formulario');
-formulario.addEventListener('submit', function (event) {
+const formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", function (event) {
   event.preventDefault();
-  const peso = Number(document.getElementById('peso').value);
-  const idade = Number(document.getElementById('idade').value);
-  const altura = Number(document.getElementById('altura').value);
-  const genero = seleciona('genero');
+  const peso = Number(document.getElementById("peso").value);
+  const idade = Number(document.getElementById("idade").value);
+  const altura = Number(document.getElementById("altura").value);
+  const genero = seleciona("genero");
   const dados = calcTMB(peso, idade, altura, genero);
   console.log(dados);
   /*
   dados.basal,dados.sedentario,dados.light,dados.moderado,dados.ativo,
   dados.superAtivo,dados.ganharPeso,dados.perderPeso*/
   const elementosHtml = `
-  <h2 style ="text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb;">Seu Resultado foi:</h2>
-  <p>
+  <h2 style ="text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb;" class="result">Seu Resultado foi:</h2>
+  <div class='result-list'>
 	 <ul>
 		 <li>Metabolismo Basal : ${dados.basal.toFixed(2)} cal</li>
 		 <li>Sedentário controle remoto : ${dados.sedentario.toFixed(2)} cal</li>
@@ -33,18 +21,27 @@ formulario.addEventListener('submit', function (event) {
 		 <li>Exercício regular : ${dados.ativo.toFixed(2)} cal</li>
 		 <li>Exercício 2horas por dia: ${dados.superAtivo.toFixed(2)} cal</li>
    </ul>
-   </p>
-   <h2 style ="text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb;">Ganhar Peso</h2>
+   </div>
+   <div>
+   <h3 style ="text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb;">Ganhar Peso</h3>
   
 	 <p>Para ganhar peso você deve consumir todos os dias ${
      dados.ganharPeso
    } cal</p>
-   <h2 style ="text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb;">Perder Peso</h2>
+   </div>
+
+   <div>
+   <h3 style ="text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb;">Perder Peso</h3>
    
-	 <p>Para perder peso você deve ter um consumo de até ${dados.perderPeso} cal</p>
+   <p>Para perder peso você deve ter um consumo de até ${
+     dados.perderPeso
+   } cal</p>
+   </div>
+   
   `;
-  const resultado = document.getElementById('resultado');
+  const resultado = document.getElementById("resultado");
   resultado.innerHTML = elementosHtml;
+  resultado.style.transition = "all .5s";
 });
 //pega o genero se masculino ou feminino
 function seleciona(id) {
@@ -56,7 +53,7 @@ function seleciona(id) {
 de acordo com a prática esportiva*/
 function calcTMB(peso, idade, altura, genero) {
   const res =
-    genero === 'Masculino'
+    genero === "Masculino"
       ? 10 * peso + 6.25 * altura - 5 * idade + 5
       : 10 * peso + 6.25 * altura - 5 * idade - 161;
   const resData = {
